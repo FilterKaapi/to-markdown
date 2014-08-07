b03f3fcb881c00d24010a1d85c0680f3964a470e
Sandeep Laxman <sandeep.laxman@gmail.com>
2014-08-07 19:06:07 +0530

    Update to-markdown.js

--- src/to-markdown.js
+++ src/to-markdown.js
@@ -84,7 +84,7 @@ var toMarkdown = function(string) {
      {
       patterns: 'fmath',
       replacement: function(str, attrs, innerHTML) {
-        attrs = attrs.replace(/\t/gi,'\\t');
+        attrs = attrs.replace(/\t/gi,'\\t').replace(/[\b]/gi,'\\b');
         var alttext = attrs.match(attrRegExp('alttext')).replace('able','\\table').replace('ext','\\text');
         return '$ ' + alttext[1] + ' $';
       }
